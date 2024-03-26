7fad3d26e115ba13e94b842adb38ad7dccd65578
freitas022 <mf022rj@gmail.com>
2024-03-26 13:46:15 -0300

    ms-api-gateway

---
+++ ms-api-gateway/src/main/java/freitas/dev/msapigateway/MsApiGatewayApplication.java
@@ -0,0 +1,15 @@
+package freitas.dev.msapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class MsApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsApiGatewayApplication.class, args);
+	}
+
+}
---
+++ ms-api-gateway/src/test/java/freitas/dev/msapigateway/MsApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package freitas.dev.msapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
